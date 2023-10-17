427903566d28c3d02ac9c7fe823b27f95c6d6ee5
Davi Costa <davim.costa@outlook.com>
2023-10-16 22:53:52 -0300

    :tada: Ínicio do projeto

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Button, Linking } from 'react-native';
+import { CLIENT_ID} from "@env";
+console.log(`CLIENT_ID: ${CLIENT_ID}`);
+const client_id = process.env.CLIENT_ID;
+const redirect_uri = 'http://localhost:8888/callback';
+console.log(client_id)
+const scope = 'user-read-private user-read-email';
 
-export default function App() {
+const LoginScreen = () => {
+  const handleLogin = () => {
+    const authorizationUrl = `https://accounts.spotify.com/authorize?` +
+      `response_type=code` +
+      `&client_id=${client_id}` +
+      `&scope=${scope}` +
+      `&redirect_uri=${redirect_uri}`;
+
+    // Redirecionar o usuário para a URL de autorização do Spotify
+    // Isso pode ser feito usando um navegador da web integrado ou uma biblioteca de navegação como 'react-navigation'.
+    // Por exemplo, usando o Linking do React Native:
+    Linking.openURL(authorizationUrl);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button title="Login with Spotify" onPress={handleLogin} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default LoginScreen;
